d907f0b345e71229e3a1bf5f4abe1cc0f3a1fba6
Austin, Noel <NAustin@kforce.com>
2017-10-25 15:25:43 -0400

    adding sample files

---
+++ Modules/HelloNancy.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Nancy;
+
+namespace SerilogAutofacApiTemplate.Controllers
+{
+    public class HelloNancy : NancyModule
+    {
+        public HelloNancy()
+        {
+            Get("/", args => "Hello World");
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using Serilog;
+using Serilog.Events;
+
+namespace SerilogAutofacApiTemplate
+{
+    public class Program
+    {
+        public static int Main(string[] args)
+        {
+            BuildWebHost(args).Run();
+
+            Log.Logger = new LoggerConfiguration()
+           .MinimumLevel.Debug()
+           .MinimumLevel.Override("Microsoft", LogEventLevel.Information)
+           .Enrich.FromLogContext()
+           .WriteTo.Trace()
+           .WriteTo.Debug()
+           .CreateLogger();
+
+            try
+            {
+                Log.Information("Starting web host");
+                BuildWebHost(args).Run();
+                return 0;
+            }
+            catch (Exception ex)
+            {
+                Log.Fatal(ex, "Host terminated unexpectedly");
+                return 1;
+            }
+            finally
+            {
+                Log.CloseAndFlush();
+            }
+
+        }
+
+        public static IWebHost BuildWebHost(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>()
+                .UseSerilog()
+                .Build();
+
+    }
+}
